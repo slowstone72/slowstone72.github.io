ebdbc608d689c6299eaef855b81ba6c335c4eafc
Callum Fisher <cf.fisher.bham@gmail.com>
2025-01-13 18:03:23 +0000

    Standardize tabs.

--- assets/js/misc/pageFindAutoSearch.js
+++ assets/js/misc/pageFindAutoSearch.js
@@ -1,16 +1,16 @@
 /* Copyright (©) 2024 Callum Fisher - cf.fisher.bham@gmail.com
 pageFindAutoSearch.js - Automatically triggers a Pagefind (https://pagefind.app/) search for the URL path content.
-2024.08.26 */
+2024.08.26 - 2025.01.13 */
 
 window.addEventListener("load", () => {
-    let pageFindSearch = new PagefindUI({
-        element: "#pageFindSearch",
-        showSubResults: true,
-        showImages: false,
-        translations: {
-            placeholder: "Type here to search"
-        }
-    });
-    let paths = location.pathname.split("/");
-    pageFindSearch.triggerSearch((`${paths[paths.length - 1]}${paths[paths.length - 2] ? " " + paths[paths.length - 2] : ""}`).replaceAll("index.html", "").trim());
+	let pageFindSearch = new PagefindUI({
+		element: "#pageFindSearch",
+		showSubResults: true,
+		showImages: false,
+		translations: {
+			placeholder: "Type here to search"
+		}
+	});
+	let paths = location.pathname.split("/");
+	pageFindSearch.triggerSearch((`${paths[paths.length - 1]}${paths[paths.length - 2] ? " " + paths[paths.length - 2] : ""}`).replaceAll("index.html", "").trim());
 });
