78bd0399b02e18ed2c713e705571a98231384543
Callum Fisher <cf.fisher.bham@gmail.com>
2025-01-14 19:33:26 +0000

    Update favicon.

---
+++ a/js/pageFindInit.js
@@ -0,0 +1,13 @@
+/* Copyright (©) 2025 Callum Fisher - cf.fisher.bham@gmail.com
+2025.01.14 */
+
+window.addEventListener('load', () => {
+	new PagefindUI({
+		element: '#pageFindSearch',
+		showSubResults: false,
+		showImages: false,
+		translations: {
+			placeholder: Math.random() < 0.20 ? 'You can search with this thing' : 'Type here to search'
+		}
+	});
+});
