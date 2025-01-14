b9290836d6a51bb0cb49a4d0ba80a0614989483f
Callum Fisher <cf.fisher.bham@gmail.com>
2025-01-14 18:48:44 +0000

    Update hamburgerMenuToggle.js

--- a/js/hamburgerMenuToggle.js
+++ a/js/hamburgerMenuToggle.js
@@ -1,11 +1,10 @@
 /* Copyright (©) 2024-2025 Callum Fisher - cf.fisher.bham@gmail.com
-2024.11.07 - 2025.01.013 */
+2024.11.07 - 2025.01.14 */
 
 let toggleHamburgerMenu;
 
 window.addEventListener('load', () => {
 	toggleHamburgerMenu = () => {
-		// let menuItems = document.getElementById('menuItems');
 		document.getElementById('menuItems').classList.toggle('active');
 	}
 });
