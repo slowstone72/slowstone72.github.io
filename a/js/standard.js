a777cbc706812175205b84e8284788ad34b3cffe
Callum Fisher <cf.fisher.bham@gmail.com>
2025-01-17 19:49:12 +0000

    Update standard.js

--- a/js/standard.js
+++ a/js/standard.js
@@ -2,6 +2,7 @@
 2025.01.17 */
 
 window.addEventListener('load', () => {
+	if (typeof localStorage.enableTracking === 'undefined') localStorage.setItem('enableTracking', 'true');
 	if (localStorage.getItem('enableTracking') === 'true') {
 		(function(c, l, a, r, i, t, y) { // Microsoft Clarity for stats:
 			c[a] = c[a] || function() { (c[a].q = c[a].q || []).push(arguments) };
@@ -9,11 +10,10 @@ window.addEventListener('load', () => {
 			y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
 		})(window, document, 'clarity', 'script', 'pvf9bh18n4');
 		let footer = document.querySelector('footer');
-		if (footer) {
-			let settings = `/settings.html`;
-			let statNotice = document.createElement('p'); 
-			statNotice.innerHTML = `This page uses <a target="_blank" href="https://clarity.microsoft.com/">Microsoft Clarity</a> for statistics. You can customize this on the <a href="${settings}">settings page</a>`;
-			footer.appendChild(statNotice);
-		}
+		if (!footer) return;
+		let settings = `/settings.html`;
+		let statNotice = document.createElement('p'); 
+		statNotice.innerHTML = `This page uses <a target="_blank" href="https://clarity.microsoft.com/">Microsoft Clarity</a> for statistics. You can customize this on the <a href="${settings}">settings page</a>`;
+		footer.appendChild(statNotice);
 	}
 });
