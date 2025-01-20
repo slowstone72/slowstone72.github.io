2b21dfaba7436cf3f306698177028ded28f631d8
Callum Fisher <cf.fisher.bham@gmail.com>
2025-01-20 15:31:35 +0000

    Update.

--- a/js/settings.js
+++ a/js/settings.js
@@ -1,78 +1,11 @@
-/* Copyright (©) 2024-2025 Callum Fisher - cf.fisher.bham@gmail.com
-2025.01.17 */
-
-let siteSettings;
-
-window.addEventListener('load', () => {
-	let loadSettings = () => {
-		// Analytics:
-		let toggleAnalytics = document.getElementById('toggleAnalytics');
-		if (toggleAnalytics) {
-			if (localStorage.getItem('enableAnalytics') === 'true') {
-				toggleAnalytics.innerText = 'Disable Microsoft Clarity for Analytics';
-			} else {
-				toggleAnalytics.innerText = 'Enable Microsoft Clarity for Analytics';
-			}
-		}
-		// Ads:
-		let toggleAds = document.getElementById('toggleAds');
-		if (toggleAds) {
-			if (localStorage.getItem('enableAds') === 'true') {
-				toggleAds.innerText = 'Disable Google AdSense for Advertising';
-			} else {
-				toggleAds.innerText = 'Enable Google AdSense for Advertising';
-			}
-		}
-		// Cookies:
-		let toggleCookies = document.getElementById('toggleCookies');
-		if (toggleCookies) {
-			if (localStorage.getItem('cookiesAccepted') === 'true') {
-				toggleCookies.innerText = 'Revoke cookie permission';
-			} else {
-				toggleCookies.innerText = 'Grant cookie permission';
-			}
-		}
-		if (localStorage.getItem('cookiesAccepted') !== 'true') {
-			if (toggleAnalytics) toggleAnalytics.classList.add('inactive');
-			if (toggleAds) toggleAds.classList.add('inactive');
-		}
-	}
-	siteSettings = {
-		toggleAnalytics: () => {
-			if (localStorage.getItem('cookiesAccepted') !== 'true') return;
-			let toggleAnalyticsButton = document.getElementById('toggleAnalytics');
-			if (localStorage.getItem('enableAnalytics') === 'true') {
-				localStorage.setItem('enableAnalytics', 'false');
-				toggleAnalyticsButton.innerText = 'Enable Microsoft Clarity for Analytics';
-			} else {
-				localStorage.setItem('enableAnalytics', 'true');
-				toggleAnalyticsButton.innerText = 'Disable Microsoft Clarity for Analytics';
-			}
-			location.reload();
-		},
-		toggleAds: () => {
-			if (localStorage.getItem('cookiesAccepted') !== 'true') return;
-			let toggleAds = document.getElementById('toggleAds');
-			if (localStorage.getItem('enableAds') === 'true') {
-				localStorage.setItem('enableAds', 'false');
-				toggleAds.innerText = 'Enable Google AdSense for Advertising';
-			} else {
-				localStorage.setItem('enableAds', 'true');
-				toggleAds.innerText = 'Disable Google AdSense for Advertising';
-			}
-			location.reload();
-		},
-		toggleCookies: () => {
-			let toggleCookies = document.getElementById('toggleCookies');
-			if (localStorage.getItem('cookiesAccepted') === 'true') {
-				localStorage.setItem('cookiesAccepted', 'false');
-				toggleCookies.innerText = 'Revoke cookie permission';
-			} else {
-				localStorage.setItem('cookiesAccepted', 'true');
-				toggleCookies.innerText = 'Grant cookie permission';
-			}
-			location.reload();
-		}
-	}
-	loadSettings();
-});
+let siteSettings;window.addEventListener('load',()=>{let load=()=>{let toggleAnalytics=document.getElementById('toggleAnalytics');if(toggleAnalytics){if(localStorage.getItem('enableAnalytics')==='true'){toggleAnalytics.innerText='Disable Microsoft Clarity for Analytics'}else{toggleAnalytics.innerText='Enable Microsoft Clarity for Analytics'}}
+let toggleAds=document.getElementById('toggleAds');if(toggleAds){if(localStorage.getItem('enableAds')==='true'){toggleAds.innerText='Disable Google AdSense for Advertising'}else{toggleAds.innerText='Enable Google AdSense for Advertising'}}
+let toggleCmts=document.getElementById('toggleCmts');if(toggleCmts){if(localStorage.getItem('enableCmts')==='true'){toggleCmts.innerText='Disable Disqus for Comments'}else{toggleCmts.innerText='Enable Disqus for Comments'}}
+let toggleCookies=document.getElementById('toggleCookies');if(toggleCookies){if(localStorage.getItem('cookiesAccepted')==='true'){toggleCookies.innerText='Revoke cookie permission'}else{toggleCookies.innerText='Grant cookie permission'}}
+if(localStorage.getItem('cookiesAccepted')!=='true'){if(toggleAnalytics)toggleAnalytics.classList.add('inactive');if(toggleAds)toggleAds.classList.add('inactive');if(toggleCmts)toggleCmts.classList.add('inactive');}}
+siteSettings={toggleAnalytics:()=>{if(localStorage.getItem('cookiesAccepted')!=='true')return;let btn=document.getElementById('toggleAnalytics');if(localStorage.getItem('enableAnalytics')==='true'){localStorage.setItem('enableAnalytics','false');btn.innerText='Enable Microsoft Clarity for Analytics'}else{localStorage.setItem('enableAnalytics','true');btn.innerText='Disable Microsoft Clarity for Analytics'}
+location.reload()},toggleAds:()=>{if(localStorage.getItem('cookiesAccepted')!=='true')return;let btn=document.getElementById('toggleAds');if(localStorage.getItem('enableAds')==='true'){localStorage.setItem('enableAds','false');btn.innerText='Enable Google AdSense for Advertising'}else{localStorage.setItem('enableAds','true');btn.innerText='Disable Google AdSense for Advertising'}
+location.reload()},toggleCmts:()=>{if(localStorage.getItem('cookiesAccepted')!=='true')return;let btn=document.getElementById('toggleCmts');if(localStorage.getItem('enableCmts')==='true'){localStorage.setItem('enableCmts','false');btn.innerText='Enable Disqus for Comments'}else{localStorage.setItem('enableCmts','true');btn.innerText='Disable Disqus for Comments'}
+location.reload()},toggleCookies:()=>{let btn=document.getElementById('toggleCookies');if(localStorage.getItem('cookiesAccepted')==='true'){localStorage.setItem('cookiesAccepted','false');btn.innerText='Revoke cookie permission'}else{localStorage.setItem('cookiesAccepted','true');btn.innerText='Grant cookie permission'}
+location.reload()}}
+load()})
--- a/js/standard-unminified.js
+++ a/js/standard-unminified.js
@@ -6,70 +6,69 @@ var disqus_config;
 window.addEventListener('load', () => {
 	if (window.location.toString().includes('/meta/privacy') || window.location.toString().includes('/meta/settings')) return;
 	let load = () => {
+		let footer = document.querySelector('footer');
 		if (typeof localStorage.enableAnalytics === 'undefined') localStorage.setItem('enableAnalytics', 'true');
 		if (localStorage.getItem('enableAnalytics') === 'true') {
-			(function(c, l, a, r, i, t, y) { // Microsoft Clarity for stats:
+			(function(c, l, a, r, i, t, y) { // Microsoft Clarity for analytics:
 				c[a] = c[a] || function() { (c[a].q = c[a].q || []).push(arguments) };
 				t = l.createElement(r); t.async = 1; t.src = 'https://www.clarity.ms/tag/' + i;
 				y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
 			})(window, document, 'clarity', 'script', 'pvf9bh18n4');
-			let footer = document.querySelector('footer');
 			if (!footer) return;
-			let privacy = '/meta/privacy/index.html';
-			let statNotice = document.createElement('p'); 
-			statNotice.innerHTML = `This page uses <a target="_blank" href="https://clarity.microsoft.com/">Microsoft Clarity</a> for analytics.<br>For more information, please refer to the <a href="${privacy}">privacy page</a>.`;
-			footer.appendChild(statNotice);
-		} else {
-			let footer = document.querySelector('footer');
-			if (!footer) return;
-			let privacy = '/meta/privacy/index.html';
-			let statNotice = document.createElement('p'); 
-			statNotice.innerHTML = `<a href="${privacy}">Privacy Policy</a>`;
-			footer.appendChild(statNotice);
+			let notice = document.createElement('p'); 
+			notice.innerHTML = `This page uses <a target="_blank" href="https://clarity.microsoft.com/">Microsoft Clarity</a> for analytics.<br>For more information, please refer to the <a href="/meta/privacy/index.html">privacy page</a>.`;
+			footer.appendChild(notice);
+		} else if (footer) {
+			let notice = document.createElement('p'); 
+			notice.innerHTML = `<a href="/meta/privacy/index.html">Privacy Policy</a>`;
+			footer.appendChild(notice);
 		} // Google AdSense:
-		/* if (typeof localStorage.enableAds === 'undefined') localStorage.setItem('enableAds', 'true');
-		if (localStorage.enableAds !== 'true' || window.location.toString().includes('/writing/')) return;
-		let googleAds = document.createElement('script');
-		googleAds.async = true;
-		googleAds.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2986674631207548';
-		googleAds.crossOrigin = 'anonymous';
-		document.head.appendChild(googleAds); */
-		/* Disqus Comments: */ if (!window.location.pathname.includes('/tech/')) return;
-		let canonicalLink = document.querySelector('link[rel="canonical"]');
-		if (!canonicalLink) {
-			canonicalLink = document.createElement('link');
-			canonicalLink.setAttribute('rel', 'canonical');
-			document.head.appendChild(canonicalLink);
+		if (typeof localStorage.enableAds === 'undefined') localStorage.setItem('enableAds', 'true');
+		if (localStorage.enableAds === 'true' && !window.location.pathname.startsWith('/writing')) {
+			let googleAds = document.createElement('script');
+			googleAds.async = true;
+			googleAds.src = 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2986674631207548';
+			googleAds.crossOrigin = 'anonymous';
+			document.head.appendChild(googleAds);
+		} // Disqus Comments:
+		if (typeof localStorage.enableCmts === 'undefined') localStorage.setItem('enableCmts', 'true');
+		if (localStorage.enableCmts === 'true' && window.location.pathname.startsWith('/tech')) {
+			let canonical = document.querySelector('link[rel="canonical"]');
+			if (!canonical) {
+				canonical = document.createElement('link');
+				canonical.setAttribute('rel', 'canonical');
+				document.head.appendChild(canonical);
+			}
+			canonical.setAttribute('href', window.location.href.replace(/\/index\.html$/, '/').replace(/\/$/, '/'));
+			let path = document.location.pathname.replace(/\/index\.html$/, '').replace(/\/$/, '');
+			if (!path.endsWith('/')) path += '/';
+			/* console.log('Disqus URL:', document.location.origin + path);
+			console.log('Disqus Identifier:', path);
+			console.log('Disqus Title:', document.title); */
+			disqus_config = function () {
+				this.page.url = document.location.origin + path;
+				this.page.identifier = path;
+				this.page.title = document.title;
+			}		
+			let div = document.createElement('div');
+			div.className = 'box';
+			div.id = 'disqus_thread';
+			let script = document.createElement('script');
+			script.src = 'https://callumfisher.disqus.com/embed.js';
+			script.setAttribute('data-timestamp', + new Date());
+			div.appendChild(script);
+			let main = document.querySelector('main');
+			if (main) main.appendChild(div);
 		}
-		canonicalLink.setAttribute('href', window.location.href.replace(/\/index\.html$/, '/').replace(/\/$/, '/'));
-		disqus_config = function () {
-			var currentPath = document.location.pathname.replace(/\/index\.html$/, '').replace(/\/$/, '');
-			if (!currentPath.endsWith('/')) currentPath += '/';
-			console.log('Disqus URL:', document.location.origin + currentPath);
-			console.log('Disqus Identifier:', currentPath);
-			console.log('Disqus Title', document.title);
-			this.page.url = document.location.origin + currentPath;
-			this.page.identifier = currentPath;
-			this.page.title = document.title;
-		}		
-		let disqusContainer = document.createElement('div');
-		disqusContainer.className = 'box';
-		disqusContainer.id = 'disqus_thread';
-		let script = document.createElement('script');
-		script.src = 'https://callumfisher.disqus.com/embed.js';
-		script.setAttribute('data-timestamp', + new Date());
-		disqusContainer.appendChild(script);
-		let mainDiv = document.querySelector('main');
-		if (mainDiv) mainDiv.appendChild(disqusContainer);
 	}
 	if (typeof localStorage.cookiesAccepted !== 'undefined' && localStorage.cookiesAccepted === 'true') {
 		load();
 	} else if (typeof localStorage.cookiesAccepted === 'undefined') {
 		let banner = document.createElement('div');
 		banner.classList.add('cookieBanner');
-		let message = document.createElement('p');
-		message.innerHTML = 'This site uses cookies to provide personalized advertisements and analyze site usage. Additionally, <a href="https://disqus.com/">Disqus</a> is used to provide comments on some pages and may use cookies. For more information, see the <a target="_blank" href="/meta/privacy/index.html">privacy page</a>.';
-		banner.appendChild(message);
+		let msg = document.createElement('p');
+		msg.innerHTML = 'This site uses cookies to provide personalized advertisements and analyze site usage. Additionally, <a href="https://disqus.com/">Disqus</a> is used to provide comments on some pages and may use cookies. For more information, see the <a target="_blank" href="/meta/privacy/index.html">privacy page</a>.';
+		banner.appendChild(msg);
 		let yes = document.createElement('a');
 		yes.classList.add('button', 'background');
 		yes.textContent = 'Accept';
