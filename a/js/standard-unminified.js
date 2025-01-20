b48eef90e718259d7dd8e78e976591f75125582b
Callum Fisher <cf.fisher.bham@gmail.com>
2025-01-20 04:05:18 +0000

    Update.

--- a/js/standard-unminified.js
+++ a/js/standard-unminified.js
@@ -33,14 +33,13 @@ window.addEventListener('load', () => {
 		googleAds.crossOrigin = 'anonymous';
 		document.head.appendChild(googleAds); */
 		/* Disqus Comments: */ if (!window.location.pathname.includes('/tech/')) return;
-		let baseUrl = window.location.origin ? window.location.origin : window.location.protocol + "//" + window.location.host;
+		/* let baseUrl = window.location.origin ? window.location.origin : window.location.protocol + "//" + window.location.host;
 		let normalizedPath = window.location.pathname;
 		normalizedPath = normalizedPath.replace(/\/index\.html$/, '/');
-		if (!normalizedPath.endsWith('/')) normalizedPath = normalizedPath + '/';
+		if (!normalizedPath.endsWith('/')) normalizedPath = normalizedPath + '/'; */
 		var disqus_config = function () {
-			if (!baseUrl) return;
-			this.page.url = baseUrl + normalizedPath;
-			this.page.identifier = normalizedPath;
+			this.page.url = document.location.href;
+			this.page.identifier = document.location.pathname.replace(/\/index\.html$/, '/');
 		}
 		let disqusContainer = document.createElement('div');
 		disqusContainer.className = 'box';
